f0d40241c9f890f20c01dbb764183da3c43fe12f
Jefferson Neres <dev.jefferson.m@gmail.com>
2022-07-01 10:03:54 -0300

    fix count views and find by id

--- src/modules/content/content.service.ts
+++ src/modules/content/content.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ApolloError } from 'apollo-server-errors';
 import { AuthUser } from 'src/infra/auth/current-user';
 import { PrismaService } from '../../infra/database/prisma/prisma.service';
 import { Role } from '../../common/enums/role.enum';
@@ -25,33 +26,49 @@ export class ContentService {
     const content = await this.prisma.contents.findUnique({ where: { id } });
 
     if (!content) {
-      return;
+      throw new ApolloError('Content not exists');
     }
 
     await this.prisma.contents.update({
       where: { id },
-      data: { views: content.views++ },
+      data: { views: content.views + 1 },
     });
 
     return content;
   }
 
-  update(user: AuthUser, id: string, updateContentInput: UpdateContentInput) {
+  async update(
+    user: AuthUser,
+    id: string,
+    updateContentInput: UpdateContentInput,
+  ) {
     if (user.role !== Role.ADMIN) {
       throw new UnauthorizedException('Unauthorized');
     }
 
+    const content = await this.prisma.contents.findUnique({ where: { id } });
+
+    if (!content) {
+      throw new ApolloError('Content not exists');
+    }
+
     return this.prisma.contents.update({
-      where: { id },
+      where: { id: content.id },
       data: updateContentInput,
     });
   }
 
-  remove(user: AuthUser, id: string) {
+  async remove(user: AuthUser, id: string) {
     if (user.role !== Role.ADMIN) {
       throw new UnauthorizedException('Unauthorized');
     }
 
-    return this.prisma.contents.delete({ where: { id } });
+    const content = await this.prisma.contents.findUnique({ where: { id } });
+
+    if (!content) {
+      throw new ApolloError('Content not exists');
+    }
+
+    return this.prisma.contents.delete({ where: { id: content.id } });
   }
 }
